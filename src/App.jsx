0b78fb5a728d575ee5edd42c5e3ba38dc95a09f3
emonsingha209 <emonsingha209@gmail.com>
2024-02-22 03:39:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <h2>Table Title </h2>
+        <div>
+          
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
